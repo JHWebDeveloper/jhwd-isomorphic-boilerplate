391be07d3554d8c8ad90b77c4925105b549aaed1
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-11-10 17:16:06 -0500

    added script to refresh browser after nodemon restarts server, also consolidated development mode conditionals to variable dev

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import helmet from 'helmet'
-import path from 'path'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { matchRoutes } from 'react-router-config'
@@ -10,12 +9,13 @@ import HTMLTemplate from '../shared/components/HTMLTemplate'
 
 const app  = express()
 const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV === 'development'
 
 app.use(helmet({
 	contentSecurityPolicy: false
 }))
 
-if (process.env.NODE_ENV === 'development') {
+if (dev) {
 	const config = require('../../webpack.dev.js')
 	const compiler = require('webpack')(config)
 
@@ -24,13 +24,9 @@ if (process.env.NODE_ENV === 'development') {
 	}))
 
 	app.use(require('webpack-hot-middleware')(compiler))
-	
-	app.use(express.static(path.join(__dirname, 'src', 'client')))
 }
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client'))
-}
+app.use(express.static('client'))
 
 app.get('*', (req, res) => {
 	const branch = matchRoutes(routes, req.url)
@@ -47,4 +43,8 @@ app.get('*', (req, res) => {
 	})
 })
 
-app.listen(port, () => console.log(`Server listening on port ${port}`)) // eslint-disable-line no-console
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`) // eslint-disable-line no-console
+
+	if (dev) require('./reload_browser').reload(port)
+})
